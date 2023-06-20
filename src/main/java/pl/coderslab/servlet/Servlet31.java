44c269bce11211dcd2046175ff3accccbcafcaa4
Łukasz N <lnawrocki84@gmail.com>
2023-06-20 23:06:30 +0200

    Initial commit

---
+++ src/main/java/pl/coderslab/servlet/Servlet31.java
@@ -0,0 +1,21 @@
+package pl.coderslab.servlet;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@WebServlet("/servlet31")
+public class Servlet31 extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        resp.setContentType("text/html;charset=utf8");
+        resp.getWriter().println("Działa");
+    }
+}
